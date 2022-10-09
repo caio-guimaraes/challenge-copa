b1353624eb778a0cf591d63648114be69ebad151
Caio Guimarães <caio23guimaraes@gmail.com>
2022-10-09 18:01:13 -0300

    Adiciona botão de sair na página profile

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,34 +1,50 @@
+import { Navigate } from 'react-router-dom'
+import { useLocalStorage } from 'react-use'
+
 import { Icon, Card, DateSelect } from '@/components'
 
-export const Profile = () => (
-    <>
-        <header className="bg-red-500 text-white p-4">
-            <div className="container max-w-3xl flex justify-between">
-                <img src="/imgs/logo-fundo-vermelho.svg" alt="" className="w-28 md:w-40" />
-            </div>
-        </header>
+export const Profile = () => {
+    const [auth, setAuth] = useLocalStorage('auth', {})
 
-        <main className='space-y-6'>
-            <section id="header" className="space-y-6 bg-red-500 text-white p-4">
-                <div className="container max-w-3xl space-y-2">
-                    <a href="/dashboard">
-                        <Icon name="back" className="w-6" />
-                    </a>
-                    <h3 className='text-2xl font-bold'>Caio Guimarães</h3>
+    const logout = () => setAuth({})
+
+    if (!auth?.user?.id) {
+        return <Navigate to="/" replace={true} />
+    }
+
+    return (
+        <>
+            <header className="bg-red-500 text-white p-4">
+                <div className="container max-w-3xl flex justify-between p-4">
+                    <img src="/imgs/logo-fundo-vermelho.svg" alt="" className="w-28 md:w-40" />
+                    <div onClick={logout} className="p-2 cursor-pointer">
+                        Sair
+                    </div>
                 </div>
-            </section>
+            </header>
 
-            <section id="content" className="container max-w-3xl p-4 space-y-4">
-                <h2 className="text-red-500 text-xl font-bold">Seus palpites</h2>
+            <main className='space-y-6'>
+                <section id="header" className="space-y-6 bg-red-500 text-white p-4">
+                    <div className="container max-w-3xl space-y-2">
+                        <a href="/dashboard">
+                            <Icon name="back" className="w-6" />
+                        </a>
+                        <h3 className='text-2xl font-bold'>Caio Guimarães</h3>
+                    </div>
+                </section>
 
-                <DateSelect />
+                <section id="content" className="container max-w-3xl p-4 space-y-4">
+                    <h2 className="text-red-500 text-xl font-bold">Seus palpites</h2>
 
-                <div className="space-y-4">
-                    <Card timeA={{ slug: 'sui'}} timeB={{ slug: 'cam'}} match={{ time: '7:00' }}/>
-                    <Card timeA={{ slug: 'uru'}} timeB={{ slug: 'cor'}} match={{ time: '7:00' }}/>
-                    <Card timeA={{ slug: 'por'}} timeB={{ slug: 'gan'}} match={{ time: '7:00' }}/>
-                </div>
-            </section>
-        </main>
-    </>
-)
+                    <DateSelect />
+
+                    <div className="space-y-4">
+                        <Card timeA={{ slug: 'sui'}} timeB={{ slug: 'cam'}} match={{ time: '7:00' }}/>
+                        <Card timeA={{ slug: 'uru'}} timeB={{ slug: 'cor'}} match={{ time: '7:00' }}/>
+                        <Card timeA={{ slug: 'por'}} timeB={{ slug: 'gan'}} match={{ time: '7:00' }}/>
+                    </div>
+                </section>
+            </main>
+        </>
+    )
+}
